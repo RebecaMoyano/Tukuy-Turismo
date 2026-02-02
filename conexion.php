93903fb3617daa6d558f16e47d0826ed20a5c8e2
RebecaMoyano <rebemoyano18@gmail.com>
2026-02-02 23:24:06 +0100

    feat: Credenciales omitidas por seguridad

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost"; 
-$usuario = "root";       
-$password = "";        
-$bbdd = "tukuy_db";      
+$servidor = "sql306.infinityfree.com"; 
+$usuario = ""; //contraseña vacía por seguridad       
+$password = "EduxsVUdXr6";        
+$bbdd = "if0_41057389_tukuy_db";      
 
 $conn = mysqli_connect($servidor, $usuario, $password, $bbdd);
 if (!$conn) {
